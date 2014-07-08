ed48c9046a158aa4572afcca2053d1c57bebf2df
cabel152 <carl.belanger.2@ulaval.ca>
2014-07-07 23:14:19 -0400

    Fix example with changes in TP2.pdf and extract some constance

--- test/tp2crypto/Example.java
+++ test/tp2crypto/Example.java
@@ -4,26 +4,35 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Example {
+    private static final String MESSAGE_1 = "30161";
+    private static final String MESSAGE_2 = "12150 www.desjardins.com VERISIGN 2025 01 01 23 3811 6069";
+    private static final String MESSAGE_3 = "2302";
+    private static final String MESSAGE_4 = "b4INtb7X6";
+    private static final String MESSAGE_5 = "x3I9AAqH4";
+    private static final String MESSAGE_6 = "bglDHJEKrWISWTYcvofgDmuiYo0ALbj3ww6yhS0";
+    private static final String MESSAGE_7 = "ovI4.HMp23D$aBbvff3Tj";
+    private static final String MESSAGE_8 = "PcO$65pVNH2vdn9ooDhR7XGtiR7PXAVOmu2GU6aaMlGnNxQ";
+    private static final String MESSAGE_9 = "lu7btIjHoMOF1Gqo9hINgn7jzjlCqhOWnNv";
+    private static final String MESSAGE_10 = "sTEkbgNNOeMJ8DwKpVg";
+    private static final String MESSAGE_11 = "lu7btIgKwSPEEpOo7hM";
     
     Client client = new Client();
     Server server = new Server();
     
     @Test
     public void Step0() {
-        client.inject(new FakeGenerator("30161"));
+        client.inject(new FakeGenerator(MESSAGE_1));
         
-        String expResult = "30161";
         String result = client.receive(null);
-        assertEquals(expResult, result);
+        assertEquals(MESSAGE_1, result);
     }
     
     @Test
     public void Step1() {
         server.inject(new FakeGenerator("12150"));
         
-        String expResult = "12150 www.desjardins.com VERISIGN 2025 01 01 23 3811 6069";
-        String result = server.receive("30161");
-        assertEquals(expResult, result);
+        String result = server.receive(MESSAGE_1);
+        assertEquals(MESSAGE_2, result);
     }
 
     @Test
@@ -31,23 +40,19 @@ public class Example {
         client.setStatus("connected");
         client.inject(new FakeGenerator("1716"));
         
-        String message = "12150 www.desjardins.com VERISIGN 2025 01 01 23 3811 6069";
-        
-        String expResult = "2302";
-        String result = client.receive(message);
-        assertEquals(expResult, result);
+        String result = client.receive(MESSAGE_2);
+        assertEquals(MESSAGE_3, result);
     }
     
     @Test
     public void Step3() {
         server.setStatus("connected");
-        server.setNc("30161");
+        server.setNc(MESSAGE_1);
         server.setNs("12150");
         server.inject(new FakeGenerator("b4INtb"));
         
-        String expResult = "b4INtb7X6";
-        String result = server.receive("2302");
-        assertEquals(expResult, result);
+        String result = server.receive(MESSAGE_3);
+        assertEquals(MESSAGE_4, result);
     }
     
     @Test
@@ -55,26 +60,24 @@ public class Example {
         
         client.setStatus("negotiating");
         initKc();
-        client.setM2("12150 www.desjardins.com VERISIGN 2025 01 01 23 3811 6069");
-        client.setM3("2302");
+        client.setM2(MESSAGE_2);
+        client.setM3(MESSAGE_3);
         client.inject(new FakeGenerator("x3I9AA"));
         
-        String expResult = "x3I9AAqH4";
-        String result = client.receive("b4INtb7X6");
-        assertEquals(expResult, result);
+        String result = client.receive(MESSAGE_4);
+        assertEquals(MESSAGE_5, result);
     }
     
     @Test
     public void Step5() {
         server.setStatus("trusted");
         initKs();
-        server.setM3("2302");
-        server.setM4("b4INtb7X6");
-        server.inject(new FakeGenerator("171", "171", "bqlDHJ"));
+        server.setM3(MESSAGE_3);
+        server.setM4(MESSAGE_4);
+        server.inject(new FakeGenerator("171", "171", "bglDHJ"));
         
-        String expResult = "bqlDHJEorWISWxY3vjfWD$uiYt0UL jrwN6ZhI0";
-        String result = server.receive("x3I9AAqH4");
-        assertEquals(expResult, result);
+        String result = server.receive(MESSAGE_5);
+        assertEquals(MESSAGE_6, result);
     }
     
     @Test
@@ -85,9 +88,8 @@ public class Example {
         initKc();
         client.inject(new FakeGenerator("ovI4.H"));
         
-        String expResult = "ovI4.HMp23D$aBbvff3Tj";
-        String result = client.receive("bqlDHJEorWISWxY3vjfWD$uiYt0UL jrwN6ZhI0");
-        assertEquals(expResult, result);
+        String result = client.receive(MESSAGE_6);
+        assertEquals(MESSAGE_7, result);
     }
     
     @Test
@@ -97,9 +99,8 @@ public class Example {
         server.setNS1("171");
         server.inject(new FakeGenerator("20731", "20731", "PcO$65"));
         
-        String expResult = "PcO$65pVNH2vdn9ooDhR7XGtiR7PXAVOmu2GU6aaMlGnNxQ";
-        String result = server.receive("ovI4.HMp23D$aBbvff3Tj");
-        assertEquals(expResult, result);
+        String result = server.receive(MESSAGE_7);
+        assertEquals(MESSAGE_8, result);
     }
 
     @Test
@@ -110,9 +111,8 @@ public class Example {
         initKc();
         client.inject(new FakeGenerator("116", "116", "lu7btI"));
         
-        String expResult = "lu7btIjHoMOF1Gqo9hINgn7jzjlCqhOWnNv";
-        String result = client.receive("PcO$65pVNH2vdn9ooDhR7XGtiR7PXAVOmu2GU6aaMlGnNxQ");
-        assertEquals(expResult, result);
+        String result = client.receive(MESSAGE_8);
+        assertEquals(MESSAGE_9, result);
     }
 
     @Test
@@ -121,11 +121,10 @@ public class Example {
         initKs();
         server.setNS1("171");
         server.setNS2("20731");
-        server.inject(new FakeGenerator("sTEkbQ"));
+        server.inject(new FakeGenerator("sTEkbg"));
         
-        String expResult = "sTEkbQNNOeMR8Lwap$g";
-        String result = server.receive("lu7btIjHoMOF1Gqo9hINgn7jzjlCqhOWnNv");
-        assertEquals(expResult, result);
+        String result = server.receive(MESSAGE_9);
+        assertEquals(MESSAGE_10, result);
     }
     
     @Test
@@ -136,19 +135,19 @@ public class Example {
         initKc();
         client.inject(new FakeGenerator("116", "116", "lu7btI"));
         
-        String expResult = "lu7btIgKwSPEEpOo7hM";
-        String result = client.receive("sTEkbQNNOeMR8Lwap$g");
+        String expResult = MESSAGE_11;
+        String result = client.receive(MESSAGE_10);
         assertEquals(expResult, result);
     }
     
     private void initKc() {
-        client.setNc("30161");
+        client.setNc(MESSAGE_1);
         client.setNs("12150");
         client.setK0("1716");
     }
     
     private void initKs() {
-        server.setNc("30161");
+        server.setNc(MESSAGE_1);
         server.setNs("12150");
         server.setK0("1716");
     }
@@ -156,65 +155,54 @@ public class Example {
     
     @Test
     public void allSteps() {
-        client.inject(new FakeGenerator("30161"));
+        client.inject(new FakeGenerator(MESSAGE_1));
         
-        String expResult = "30161";
         String result = client.receive(null);
-        assertEquals(expResult, result);
+        assertEquals(MESSAGE_1, result);
         
         server.inject(new FakeGenerator("12150"));
-        expResult = "12150 www.desjardins.com VERISIGN 2025 01 01 23 3811 6069";
-        result = server.receive("30161");
-        assertEquals(expResult, result);
+        result = server.receive(MESSAGE_1);
+        assertEquals(MESSAGE_2, result);
 
 
         client.inject(new FakeGenerator("1716"));
-        String message = "12150 www.desjardins.com VERISIGN 2025 01 01 23 3811 6069";
-        expResult = "2302";
+        String message = MESSAGE_2;
         result = client.receive(message);
-        assertEquals(expResult, result);
+        assertEquals(MESSAGE_3, result);
 
         server.inject(new FakeGenerator("b4INtb"));
-        expResult = "b4INtb7X6";
-        result = server.receive("2302");
-        assertEquals(expResult, result);
+        result = server.receive(MESSAGE_3);
+        assertEquals(MESSAGE_4, result);
 
         client.inject(new FakeGenerator("x3I9AA"));
-        expResult = "x3I9AAqH4";
-        result = client.receive("b4INtb7X6");
-        assertEquals(expResult, result);
+        result = client.receive(MESSAGE_4);
+        assertEquals(MESSAGE_5, result);
 
-        server.inject(new FakeGenerator("171", "171", "bqlDHJ"));
-        expResult = "bqlDHJEorWISWxY3vjfWD$uiYt0UL jrwN6ZhI0";
-        result = server.receive("x3I9AAqH4");
-        assertEquals(expResult, result);
+        server.inject(new FakeGenerator("171", "171", "bglDHJ"));
+        result = server.receive(MESSAGE_5);
+        assertEquals(MESSAGE_6, result);
 
         client.inject(new FakeGenerator("ovI4.H"));
         client.setNumCompte("80123");
         client.setPassword("BN12Z");
-        expResult = "ovI4.HMp23D$aBbvff3Tj";
         result = client.receive("bqlDHJEorWISWxY3vjfWD$uiYt0UL jrwN6ZhI0");
-        assertEquals(expResult, result);
+        assertEquals(MESSAGE_7, result);
 
         server.inject(new FakeGenerator("20731", "20731", "PcO$65"));
-        expResult = "PcO$65pVNH2vdn9ooDhR7XGtiR7PXAVOmu2GU6aaMlGnNxQ";
-        result = server.receive("ovI4.HMp23D$aBbvff3Tj");
-        assertEquals(expResult, result);
+        result = server.receive(MESSAGE_7);
+        assertEquals(MESSAGE_8, result);
 
         client.inject(new FakeGenerator("116", "116", "lu7btI"));
-        expResult = "lu7btIjHoMOF1Gqo9hINgn7jzjlCqhOWnNv";
-        result = client.receive("PcO$65pVNH2vdn9ooDhR7XGtiR7PXAVOmu2GU6aaMlGnNxQ");
-        assertEquals(expResult, result);
+        result = client.receive(MESSAGE_8);
+        assertEquals(MESSAGE_9, result);
 
-        server.inject(new FakeGenerator("1", "1", "sTEkbQ"));
-        expResult = "sTEkbQNNOeMR8Lwap$g";
-        result = server.receive("lu7btIjHoMOF1Gqo9hINgn7jzjlCqhOWnNv");
-        assertEquals(expResult, result);
+        server.inject(new FakeGenerator("sTEkbg"));
+        result = server.receive(MESSAGE_9);
+        assertEquals(MESSAGE_10, result);
 
         client.inject(new FakeGenerator("116", "116", "lu7btI"));
-        expResult = "lu7btIgKwSPEEpOo7hM";
-        result = client.receive("sTEkbQNNOeMR8Lwap$g");
-        assertEquals(expResult, result);
+        result = client.receive(MESSAGE_10);
+        assertEquals(MESSAGE_11, result);
     }
     
 }
